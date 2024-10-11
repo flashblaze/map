fd8d221c8085ab36818d09661ed27d0d036a6ce0
flashblaze <neerajlagwankar@gmail.com>
2024-10-12 00:48:32 +0530

    log

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
 export function createDB(env: App.Locals["env"]) {
+  console.log(env);
   const client = createClient({
     url: env.TURSO_CONNECTION_URL,
     authToken: env.TURSO_AUTH_TOKEN,
