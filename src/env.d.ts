ed1ef758bde4e281e21a19b4fe1b2eeeacf9a4fb
flashblaze <neerajlagwankar@gmail.com>
2024-10-19 17:19:32 +0530

    Use D1 to test

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,15 +7,19 @@ declare namespace App {
     env: {
       TURSO_CONNECTION_URL: string;
       TURSO_AUTH_TOKEN: string;
+      CLOUDFLARE_ACCOUNT_ID: string;
+      CLOUDFLARE_DATABASE_ID: string;
+      CLOUDFLARE_D1_TOKEN: string;
     };
   }
 }
 
-interface ImportMetaEnv {
-  readonly TURSO_CONNECTION_URL: string;
-  readonly TURSO_AUTH_TOKEN: string;
-}
-
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: {
+    readonly TURSO_CONNECTION_URL: string;
+    readonly TURSO_AUTH_TOKEN: string;
+    readonly CLOUDFLARE_ACCOUNT_ID: string;
+    readonly CLOUDFLARE_DATABASE_ID: string;
+    readonly CLOUDFLARE_D1_TOKEN: string;
+  };
 }
